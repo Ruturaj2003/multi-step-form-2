9ef82f6321bba175cf9558402ff2fc7a09f991da
Ruturaj2003 <ruturajhb2003@gmail.com>
2025-02-07 19:19:59 +0530

    Setup Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import FormLayout from './pages/caneSupplierForm/FormLayout';
+import { default as CaneSupplierLayout } from './pages/caneSupplierForm/FormLayout';
+import Home from './pages/Home';
+import General from './pages/caneSupplierForm/General';
+import Address from './pages/caneSupplierForm/Address';
+import Billing from './pages/caneSupplierForm/Billing';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<FormLayout></FormLayout>}></Route>
+        <Route path="/" element={<Home></Home>}></Route>
+        {/* Cane Supplier Form */}
+        <Route
+          path="/cane-supplier"
+          element={<CaneSupplierLayout></CaneSupplierLayout>}
+        >
+          <Route index element={<General></General>}></Route>
+          <Route path="address" element={Address}></Route>
+          <Route path="billing" element={<Billing></Billing>}></Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen space-y-4">
+      <Link
+        to="/cane-supplier"
+        className="px-4 py-2 bg-blue-500 text-white rounded"
+      >
+        Cane Supplier
+      </Link>
+      <Link
+        to="/form-two"
+        className="px-4 py-2 bg-green-500 text-white rounded"
+      >
+        Go to Form 2
+      </Link>
+    </div>
+  );
+};
+export default Home;
